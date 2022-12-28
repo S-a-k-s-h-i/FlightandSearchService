9ac6d080b42dfa69adce5d66c6274a97a7441ee5
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2022-12-29 00:12:32 +0530

    Completed  City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -10,6 +10,28 @@ class CityRepository {
     }
   }
 
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateCity(data, cityId) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async deleteCity(cityId) {
     try {
       await City.destroy({
