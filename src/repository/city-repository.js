0811a44fc6b39a4ca772e21ede1937ca231609af
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2022-12-29 22:51:11 +0530

    Added filters to getAllCities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,5 @@
 const { City } = require("../models/index");
+const { Op } = require("sequelize");
 
 class CityRepository {
   async createCity({ name }) {
@@ -52,11 +53,22 @@ class CityRepository {
     }
   }
 
-  async getAllCities() {
+  async getAllCities(filter) {
     try {
+      if (filter.name) {
+        const cities = await City.findAll({
+          where: {
+            name: {
+              [Op.startsWith]: filter.name,
+            },
+          },
+        });
+        return cities;
+      }
       const cities = await City.findAll();
       return cities;
     } catch (error) {
+      console.log("Something went wrong at repository layer");
       throw error;
     }
   }
