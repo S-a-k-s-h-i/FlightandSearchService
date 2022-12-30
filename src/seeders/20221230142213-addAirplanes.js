91ce16105d88f6140bcd43cd8cb81ed5db9a3a04
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2022-12-30 20:33:32 +0530

    Added airplane model

---
+++ src/seeders/20221230142213-addAirplanes.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Airplanes",
+      [
+        {
+          model_number: "Airbus A330",
+          capacity: 300,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          model_number: "Boeing 747",
+          capacity: 400,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          model_number: "Boeing 744",
+          capacity: 350,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
